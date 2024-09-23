4016e1651aea9cbd113c8067e42ecb9929627b64
dipakgit123 <punddipak9@gmail.com>
2024-09-23 13:21:17 +0530

    added

---
+++ script.js
@@ -0,0 +1,25 @@
+let imgContainer = document.getElementById("img-container");
+
+function Akshay(){
+    imgContainer.src = "akshay.jpeg"
+}
+
+function Alia(){
+    imgContainer.src = "alia.jpeg"
+}
+
+function Virat(){
+    imgContainer.src = "virat1.jpg"
+}
+
+function Shradha(){
+    imgContainer.src = "shradha.jpg"
+}
+
+function Rohit(){
+    imgContainer.src = "sharma.jpeg"
+}
+
+function Nani(){
+    imgContainer.src = "nani.jpg"
+}
